87bdda2257c889e2286f58ac26b130069ba54c61
Alex Lopes <33962450+akelopes@users.noreply.github.com>
2023-10-11 19:45:45 -0300

    Update test_core.py

--- deployWatcher/tests/test_core.py
+++ deployWatcher/tests/test_core.py
@@ -10,14 +10,7 @@ def test_app_initialization(client):
 def test_app_post_response(client, session):
     payload = {'component': 'testApp', 'version': 1.0, 'author': 'automata7', 'status': 'started',
                'sent_timestamp': datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')}
-<<<<<<< HEAD
-    
-    response = client.post('/transitions', json=payload)
-=======
-
     response = client.post('/transitions', data=payload)
->>>>>>> efaef465f656354e52b9b2aa50fecd59db5c42ac
-
     assert response.status_code == 200
 
 
